1bf293f69cbffac1f9c1c1d1f4bdee54249717ff
listoun <hassan0outline0@gmail.com>
2023-03-11 00:28:55 +0100

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,6 @@
 
 export const Footer = () => {
-    return <div className="text-center py-7 dark:text-neutral-400 text-neutral-600 text-sm">footer</div>
+    return <div className="text-center py-7 dark:text-neutral-400 text-neutral-600 text-sm">
+        Made wtih Love and Passion ❤️‍🔥
+    </div>
 }
