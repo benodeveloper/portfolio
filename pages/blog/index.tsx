a3a2064a59e962a11e1aa7fb53b256dfdc61242a
listoun <hassan0outline0@gmail.com>
2023-03-10 23:57:37 +0100

    text bog improvment

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -10,7 +10,10 @@ type PostsProps = {
 
 const Posts = ({ posts }: PostsProps) => {
     return <div className="my-20">
-        <h2 className="text-center text-4xl font-bold mb-14 dark:text-white text-neutral-800">Blog</h2>
+        <div className="mb-14">
+            <h1 className="text-5xl mb-7 font-bold dark:text-neutral-100 text-neutral-900">Blog</h1>
+            <h4 className="dark:text-neutral-400 text-lg text-neutral-600">Here you will find my notes and tutorials for my future self, and hopefully also for you.</h4>
+        </div>
         {posts.map((post, key) => <Post post={post} key={key} />)}
     </div>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,8 +13,8 @@ const Home = ({ posts }: HomeProps): JSX.Element => {
   return <>
     <div className="my-20">
       <div className="mb-14">
-        <h2 className="text-5xl mb-7 font-bold dark:text-neutral-100 text-neutral-900">Blog</h2>
-        <h4 className="dark:text-neutral-400 text-lg text-neutral-600">Java, Spring Boot, coding, technology and beyond.</h4>
+        <h1 className="text-5xl mb-7 font-bold dark:text-neutral-100 text-neutral-900">Blog</h1>
+        <h4 className="dark:text-neutral-400 text-lg text-neutral-600">Coding, technology and beyond.</h4>
       </div>
       {posts.map((post, key) => <Post post={post} key={key} />)}
     </div>
