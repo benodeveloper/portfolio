9a37cc357eaf4e5a55c41fa82efcb1c89641d8a2
listoun <hassan0outline0@gmail.com>
2023-03-11 01:18:58 +0100

    post tag {wip}

--- components/post.tsx
+++ components/post.tsx
@@ -7,8 +7,8 @@ export const Post = ({ post }: { post: IPost }) => {
         <Link title={post.title} href={post.url}>
             <h2 className="text-2xl font-bold hover:text-indigo-500 dark:hover:text-blue-400/80 transition-colors text-neutral-800 dark:text-white">{post.title}</h2>
         </Link>
-        <div className="my-3 text-sm font-bold">
-            <span className="mr-2">tag</span> - <span className="mx-2">tag</span>
+        <div className="my-3 text-sm font-bold text-neutral-700/90 ">
+            <span className="mr-2 hover:text-indigo-500 dark:hover:text-blue-400/80">tag</span> - <span className="mx-2">tag</span>
         </div>
         <Link title={post.title} href={post.url}>
             <h3 className="text-neutral-700 dark:text-neutral-300/90 pt-2">{post.description}</h3>
