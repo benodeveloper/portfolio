fa49df1e4db73d08a85523080fa1263b57ab2bc1
benodeveloper <listoun.developer@gmail.com>
2023-08-01 21:42:56 +0100

    Add more details in Slack webhook

--- util/slackHooks.ts
+++ util/slackHooks.ts
@@ -4,16 +4,15 @@ export async function hookVisit(page: string) {
     const enabled    = process.env.NEXT_PUBLIC_SLACK_WEBHOOK_ENABLE == "true" || false;
     const webhookUrl = process.env.NEXT_PUBLIC_SLACK_WEBHOOK_URL || null;
     
-    const clientIP = await fetch('https://api.ipify.org?format=json')
+    const client = await fetch('http://ip-api.com/json')
     .then(response => response.json())
-    .then(data => data.ip);
-    const message    = `IP_ADD: ${clientIP} visited the ${page} page of your website on ${now.split("T")[0]} at ${now.split("T").pop()?.split(".")[0]}`;    
- 
+    .then(data => data)
+    .catch(err =>console.log(err));
+
+    const message    = `Client form ${client.country} with Address IP:"${client.query}" visited the ${page} page of your website on ${now.split("T")[0]} at ${now.split("T").pop()?.split(".")[0]}`;    
+    
     if(enabled && webhookUrl) {
         fetch(webhookUrl, {method: "POST", body: JSON.stringify({"text": message})})
-    } else {
-        console.log(message);
-        
     }
 
     
