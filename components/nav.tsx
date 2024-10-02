3a635620f288d0136d00a99887b4ce55df2d9ae0
grafimage <listoun.developer@gmail.com>
2024-10-02 11:40:54 +0100

    WIP

---
+++ components/nav.tsx
@@ -0,0 +1,15 @@
+const Nav = ()=> {
+    return <div>
+        <div>Ben'adardor</div>
+        <ul>
+            <li>
+                <a href="#">Home</a>
+                <a href="#">Work</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </div>
+}
+
+export default Nav;
